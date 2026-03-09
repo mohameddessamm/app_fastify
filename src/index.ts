cf4b4581954fb3dab1eb0c50d7ba971c69fb6584
mohamedessam <mohameddessamm209@gmail.com>
2026-03-09 05:16:32 +0200

    delete branch

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,9 @@ import Fastify from 'fastify';
 import fastifyCookie from '@fastify/cookie';
 import cors from '@fastify/cors';
 import { prisma } from './lib/prisma';
-<<<<<<< HEAD
-
-// استيراد المسارات
-import { userRoutes } from './routes/userRoutes';
-import { movieRoutes } from './routes/movieRoutes';
 
 
-const app = Fastify({
-    logger: {
-        transport: {
-            target: 'pino-pretty',
-            options: { colorize: true }
-        }
-    }
-});
-
-// 1. تسجيل الـ Plugins
-await app.register(cors, { 
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-=======
+// استيراد المسا
 import { movieRoutes } from "./routes/movieRoutes";
 // استيراد مجموعات المسارات
 import { authRoutes } from './routes/authRoutes';
@@ -39,7 +20,7 @@ await app.register(cors, {
     origin: 'http://localhost:5173', 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
->>>>>>> main
+
 });
 app.register(fastifyCookie);
 // في ملف server.ts أو main.ts
@@ -47,25 +28,9 @@ app.register(movieRoutes, { prefix: 'api/movies' });
 app.register(authRoutes, { prefix: 'api/auth' }); 
 // 2. تسجيل مجموعات المسارات (هنا التنظيم الحقيقي)
 
-<<<<<<< HEAD
+
 // 2. تسجيل مجموعات المسارات (Register Routes)
-app.register(userRoutes, { prefix: '/api/auth' });
-app.register(userRoutes, { prefix: '/api/create-root-admin' });
-app.register(movieRoutes, { prefix: '/api/admin' });
 
-// مسارات خاصة/سريعة
-
-app.get("/ping", async () => ({ message: "pong" }));
-
-// 3. دالة تشغيل السيرفر
-const start = async () => {
-    try {
-        await prisma.$connect();
-        console.log('✅ Database connected');
-        
-        await app.listen({ port: 4000, host: '0.0.0.0' });
-        console.log('🚀 Server ready at http://localhost:4000');
-=======
   // ستصبح الروابط /auth/login
 app.register(adminRoutes, { prefix: 'api/admin' }); // ستصبح الروابط /admin/movies
 
@@ -76,7 +41,7 @@ const start = async () => {
         await prisma.$connect();
         await app.listen({ port: 4000, host: '0.0.0.0' });
         console.log('🚀 Server running on http://localhost:4000');
->>>>>>> main
+
     } catch (err) {
         app.log.error(err);
         process.exit(1);
