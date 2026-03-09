12464b811689d1b364a5921fad5cdd42eaa6121e
mohamedessam <mohameddessamm209@gmail.com>
2026-03-09 06:17:40 +0200

    oky pull request

--- my-front-end/src/pages/user/userTsx/Register.tsx
+++ my-front-end/src/pages/user/userTsx/Register.tsx
@@ -43,7 +43,7 @@ const Register = () => {
             className={`top-nav-btn ${showRegisterForm ? "active-btn" : ""}`}
             onClick={() => setShowRegisterForm(!showRegisterForm)}
           >
-            Registerrrrrrrrrrrrrrrrr
+            Registe
           </button>
           <button
             className="top-nav-btn login-style"
